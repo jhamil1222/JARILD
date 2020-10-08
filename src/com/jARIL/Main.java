db55f7a24301eda65bec8ac78e40c438ce2bfdf6
jhamilroldan <contact>
2020-10-08 16:58:29 -0400

    MEJORA DE modificacion

--- src/com/jARIL/Main.java
+++ src/com/jARIL/Main.java
@@ -8,29 +8,13 @@ public class Main {
     public static void main(String[] args) {
     modificacion pi=new modificacion();
     //nota no podras establecer neuronas de entrada mayores a las salidas
-        double[][]x=new double[][]{{0.05,0.10}};
+        double[][]x=new double[][]{{0.05,0.10},{0,1}};
 
-        double[][]r=new double[][]{{0.75136507,0.772928465}};
-		double[][]o=new double[][]{{4,2,3},{1,2,3}};
+        double[][]r=new double[][]{{2}};
+		double[][]o=new double[][]{{2,4,3}};
         double [][]y=new double[][]{{ 0.01,0.99}};
-		//MatJa.impMat( MatJa.result(o,r));
-       // MatJa.impMat( funcion_Act.sigm(y)[1]);
-        //MatJa.impMat(error.errCua(r,y)[0]);
-      
-           // MatJa.impMat(MatJa.restonVect(y,r));
-        //MatJa.impMat(MatJa.result(x,w));
-    //MatJa.impMat(MatJa.inse1(x,1));
-       //MatJa.impMat (MatJa.ingre1(x,3));
-        /*recordar que hay que establecer una fucnion de activacion por neurona por ello
-        la nueva cadena de arrays no tendra que ser mayor a los enteros que representan el numero de
-         neuronas por capa*/
-        /* la primera capa tiene que ser igual al numero de columnas de la entrada*/
-        pi.train(new int[]{2,2,2},new String[]{"sigm","sigm","sigm","sigm","sigm","sigm"},x,y,true,0.001);
-
-        //MatJa.impMat(MatJa.result(x,w));
-        //MatJa.impMat(MatJa.MAX(MatJa.ingresEnt(x,0),x));
-        //MatJa.impMat(funcion_Act.relu(x)[0]);
-		//MatJa.impMat(MatJa.result(po,pil));
+        modificacion pul=new modificacion();
+        pul.train(new int[]{3,4,2},new String[]{"relu","relu","relu"},x,y,true,0.01);
     
     }
 }
--- src/com/jARIL/modificacion.java
+++ src/com/jARIL/modificacion.java
@@ -5,24 +5,24 @@ import java.util.ArrayList;
 public class modificacion {
 
         public void train(int[] topo,String [] act,double [][]x,double[][]y, boolean train,double lr){
-            capita poli=new capita();
+
             double [][]a=new double[topo[topo.length-1]][topo.length];
             //el proceso es el siguiente las neuronas bienen dadas el numero de neuronas que existen en la siguiente capa
             //tendras un numero de conexiones lo otro es que tendras que definir el numero de conexiones
             //en torno al numero de neuronas
             ArrayList<double[][]> polilla=new ArrayList<>();
-            ArrayList<RedNeur> pil=poli.capa(topo,act);
             ArrayList<double[][]> funciAct=new ArrayList<>();
             double [][]r=new double[topo[topo.length-1]][topo.length];
+           gatito[] ponds=multicap(x,topo);
+			
+           // MatJa.impMat(ponds.multicap(x,new int[]{2,3,5,7})[3].b);
            //cada fila de w es para cada neurona
-            double [][][]w={{{0.15,0.20},{0.25,0.30}},{{0.40,0.45},{0.50,0.55}}};
-            //definir b por neurona
-            double [][]b={{0.35},{0.60}};
             ArrayList <double [][]> polu=new ArrayList<>();
             polu.add(MatJa.memoriaFantasma(x));
-            for(int pio=0; pio<2; pio++){
+			int pio=0;
+            for(gatito pul:ponds){
 
-                double[][]z= MatJa.SumVect(MatJa.result(polu.get(pio),MatJa.matTi(w[pio])),new double[][]{b[pio]});
+                double[][]z= MatJa.SumVect(MatJa.result(polu.get(pio),pul.peso),pul.b);
                 /*aun experimental es switch de abajo falta mejorar relu y la capacidad de la red pero lo dejare como beta*/
                 switch(act[pio]){
                     case "sigm":
@@ -34,62 +34,69 @@ public class modificacion {
                         r=funcion_Act.relu(a)[1];
                         break;
                 }
+
                 polu.add(a);
                 //pos=new double[][][]{x,a};
-
-
                 funciAct.add(a);
 
 
                 polilla.add(r);
-                //MatJa.impMat(z);
+                //MatJa.impMat(MatJa.result(polu.get(pio),ponds[pio].peso));
                 //depues de depurar alrededor de 2 dias si se pudo corregir tanto result como red neur
                 //cualquier problema de claculo es culpa de MatJa.result corregir en depuracion
 
-                x=a;
-
+                //x=a;
+				pio++;
 
             }
+            double [][]nue=new double[1][1],uji=new double[1][1],ajo=new double[1][1];
 
-            //MatJa.impMat(error.errCua(funciAct.get(funciAct.size()-1),y)[1]);
-            MatJa.impMat(polilla.get(1));
+            if(train){
+                ajo=funciAct.get(funciAct.size()-1);
+                /*problemilla con este lado por que tengo que utilizar la anterior capa a
+                esta esto incluye trabajar con capas ocultas mayores a 1 */
+                if(funciAct.size()-1!=1) {
+                    uji = funciAct.get(funciAct.size() - 2);
+                }
+                MatJa.impMat(polilla.get(polilla.size()-1));
+                MatJa.impMat(MatJa.mulTiGran(error.errCua(ajo,y)[1],polilla.get(polilla.size()-1)));
 
+            }
+           // MatJa.impMat(error.errCua(funciAct.get(funciAct.size()-1),y)[0]);
 
-            /*
-            //MatJa.impMat(error.errCua(y,funciAct.get(funciAct.size()-1))[0]);
-            double [][]_w=new double[pil.get(0).pesos.length][pil.get(0).pesos[0].length];
-            double [][]al;
+            //double [][]_w=new double[pil.get(0).pesos.length][pil.get(0).pesos[0].length];
+            /*double [][]al;
 
             ArrayList<double[][]> perla=new ArrayList<>();
 
             if(train){
 
-            //primero evaluamos la ultima capa
+                //primero evaluamos la ultima capa
 
-            for(int reverso=funciAct.size()-1; reverso>=0; reverso--){
+                for(int reverso=funciAct.size()-1; reverso>=0; reverso--){
 
-                        al=funciAct.get(reverso);
-
-
-                if(reverso==funciAct.size()-1){
-
-                    perla.add(0,MatJa.mulTiGran(error.errCua(al,y)[1],polilla.get(reverso)));
-
-                }
-                else {
+                    al=funciAct.get(reverso);
 
 
-                    //MatJa.impMat(perla.get(0));
-                    //perla.add(MatJa.mulTiGran(MatJa.result(perla.get(0),MatJa.matTi(_w)),polilla.get(reverso)));
+                    if(reverso==funciAct.size()-1){
 
-                    perla.add(0,MatJa.x(MatJa.result(perla.get(0),MatJa.matTi(_w)),polilla.get(reverso)));
+                        perla.add(0,MatJa.mulTiGran(error.errCua(al,y)[1],polilla.get(reverso)));
+
+                    }
+                    else {
 
 
-                }
-                _w=MatJa.memoriaFantasma(pil.get(reverso).pesos);
-                //MatJa.GranmediaVect(new double[][]{{9,2,3},{2,2,7},{4,2,2}} );
-                //MatJa.impMat(pil.get(reverso).pesos);
-                //MatJa.impMat(_w);
+                        //MatJa.impMat(perla.get(0));
+                        //perla.add(MatJa.mulTiGran(MatJa.result(perla.get(0),MatJa.matTi(_w)),polilla.get(reverso)));
+
+                        perla.add(0,MatJa.x(MatJa.result(perla.get(0),MatJa.matTi(_w)),polilla.get(reverso)));
+
+
+                    }
+                    _w=MatJa.memoriaFantasma(pil.get(reverso).pesos);
+                    //MatJa.GranmediaVect(new double[][]{{9,2,3},{2,2,7},{4,2,2}} );
+                    //MatJa.impMat(pil.get(reverso).pesos);
+                    //MatJa.impMat(_w);
 
                 /*RedNeur pli=new RedNeur();
                 pli.umbral =MatJa.restonVect(pil.get(reverso).umbral,MatJa.result(lr,MatJa.GranmediaVect(perla.get(0))));
@@ -104,12 +111,54 @@ public class modificacion {
 
 
 
-
+	
 
 
 
 
         }
+        //arreglar despues sospechas de salida pero no preocuparse
+        public gatito [] multicap(double [][]entrada,int [] CapOc){
+            /**por ser multicapa este solo registra capas ocultas sin reditrara capa de entrada
+             * */
+            //gatito fue estableciso como la columna de CapOc por que aumentamos 1 a la entrada con
+            //la operacion de abajo que toma perro[0]
+            gatito [] perro =new gatito[CapOc.length];
+            /*permiteme explicarte novato como funciona esto perro[0]contendra la neurona que
+            viene dada por pesos dados por la entrada y la primera capa oculta*/
+            int modif=entrada[0].length,lo=CapOc[0];
+            perro[0]=new gatito();
+            perro[0].Cap(lo,modif,0,1,false);
+            //te preguntaras por que -2 muy facil no incluimos la capa de entrada como una capa
+            //solo como una posible entrada
+            
+            for(int movimiento=0,pol=1; movimiento<=CapOc.length-2;movimiento++){
+                modif=CapOc[pol];
+				if(pol==1)
+				{
+					lo=CapOc[0];
+				}
+                
+                perro[pol]=new gatito();
+                perro[pol].Cap(modif,lo,0,1,false);
+				//MatJa.impMat(perro[pol].peso);
+                //el -2 de arriba viene dado por esta calculada de aqui
+                lo=CapOc[movimiento+1];
+                pol++;
+            }
+
+
+            return perro;
+
+        }
+
 }
 
+class gatito{
+    public double [][]peso, b;
+    public void Cap(int neur,int con,double emp, double fin,boolean signo){
+        peso=MatJa.random(con,neur,emp,fin,signo);
+        b=MatJa.random(1,neur,emp,fin,signo);
+    }
+}
 
